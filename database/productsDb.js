18ec595c7e61660d885e59ca1a34ab7b3f4d77e0
Ultron <duartemattias3@gmail.com>
2023-02-18 17:21:20 +0100

    update products

--- database/productsDb.js
+++ database/productsDb.js
@@ -112,5 +112,167 @@ export const products = [
         name: 'Audifonos Bass',
         price: 200,
     },
+    {
+        category: 'celulares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 7,
+        inStock: 25,
+        images: [
+            '/iphone-13-pro-1.png'
+        ],
+        name: 'Iphone 13 Pro',
+        price: 800,
+    },
+    {
+        category: 'celulares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 8,
+        inStock: 5,
+        images: [
+            '/iphone-14-pro-1.png',
+            '/iphone-14-pro-2.png',
+        ],
+        name: 'Iphone 14 Pro',
+        price: 1000,
+    },
+    {
+        category: 'computadoras',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 9,
+        inStock: 5,
+        images: [
+            '/mac-studio-m1-1.png',
+            '/mac-studio-m1-2.png',
+        ],
+        name: 'Mac Studio M1',
+        price: 1200,
+    },
+    {
+        category: 'computadoras',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 10,
+        inStock: 10,
+        images: [
+            '/macbook-air-m2-1.png',
+            '/macbook-air-m2-2.png',
+        ],
+        name: 'MacBook Air M2',
+        price: 900,
+    },
+    {
+        category: 'consolas',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 11,
+        inStock: 14,
+        images: [
+            '/oculus-quest-2-1.png',
+            '/oculus-quest-2-2.png',
+        ],
+        name: 'Oculus Quest 2',
+        price: 600,
+    },
+    {
+        category: 'consolas',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 12,
+        inStock: 20,
+        images: [
+            '/playstation-4-1.png',
+            '/playstation-4-2.png',
+        ],
+        name: 'PlayStation 4 Slim', 
+        price: 300,
+    },
+    {
+        category: 'consolas',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 13,
+        inStock: 10,
+        images: [
+            '/playstation-5-1.png',
+            '/playstation-5-2.png',
+        ],
+        name: 'PlayStation 5', 
+        price: 550,
+    },
+    {
+        category: 'consolas',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 14,
+        inStock: 10,
+        images: [
+            '/playstation-vr-1.png',
+            '/playstation-vr-2.png',
+        ],
+        name: 'PlayStation VR', 
+        price: 500,
+    },
+    {
+        category: 'celulares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 15,
+        inStock: 50,
+        images: [
+            '/watch-1.png',
+            '/watch-2.png',
+        ],
+        name: 'SmartWatch Sport', 
+        price: 70,
+    },
+
 
 ]
