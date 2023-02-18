fac53fee5a996e93ba20e155cb3cf104eb5ff083
Mattias <duartemattias3@gmail.com>
2023-02-17 23:42:19 -0400

    adding products

--- database/productsDb.js
+++ database/productsDb.js
@@ -1,30 +1,116 @@
 export const products = [
     {
-        category: '',
-        description: '',
+        category: 'auriculares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
         id: 1,
-        inStock: 0,
-        images: [],
-        name: 'Producto 1',
-        price: 0,
+        inStock: 30,
+        images: [
+            '/earphones_a_1.png',
+            '/earphones_a_2.png',
+            '/earphones_a_3.png',
+            '/earphones_a_4.png',
+
+        ],
+        name: 'Audífonos deportivos',
+        price: 60,
     },
     {
-        category: '',
-        description: '',
+        category: 'auriculares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
         id: 2,
-        inStock: 0,
-        images: [],
-        name: 'Producto 2',
-        price: 0,
+        inStock: 20,
+        images: [
+            '/earphones_b_1.png',
+            '/earphones_b_2.png',
+
+        ],
+        name: 'Audifonos inalambricos',
+        price: 80,
     },
     {
-        category: '',
-        description: '',
+        category: 'auriculares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
         id: 3,
-        inStock: 0,
-        images: [],
-        name: 'Producto 3',
-        price: 0,
+        inStock: 25,
+        images: [
+            '/earphones_c_1.png',
+            '/earphones_c_2.png',
+
+        ],
+        name: 'Audifonos Sports',
+        price: 120,
+    },
+    {
+        category: 'auriculares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 4,
+        inStock: 10,
+        images: [
+            '/headphones-a-1.png',
+            '/headphones-a-2.png',
+        ],
+        name: 'Audifonos Gamer',
+        price: 250,
+    },
+    {
+        category: 'auriculares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 5,
+        inStock: 20,
+        images: [
+            '/headphones-b-1.png',
+            '/headphones-b-2.png',
+        ],
+        name: 'Audifonos Bass',
+        price: 200,
+    },
+    {
+        category: 'auriculares',
+        description: `Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas.
+        Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. 
+        Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. 
+        Cras convallis suscipit sodales. Curabitur quis porta velit. 
+        Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. 
+        Etiam et turpis nunc. Suspendisse potenti.
+        `,
+        id: 6,
+        inStock: 20,
+        images: [
+            '/headphones-c-1.png',
+            '/headphones-c-2.png',
+        ],
+        name: 'Audifonos Bass',
+        price: 200,
     },
 
 ]
