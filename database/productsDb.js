b4c998a7e98a69092a912868aeed78a51f5d998d
Ultron <duartemattias3@gmail.com>
2023-02-19 14:58:10 +0100

    update images products

--- database/productsDb.js
+++ database/productsDb.js
@@ -11,10 +11,10 @@ export const products = [
         id: 1,
         inStock: 30,
         images: [
-            '/earphones_a_1.png',
-            '/earphones_a_2.png',
-            '/earphones_a_3.png',
-            '/earphones_a_4.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776444/tech%20store/earphones_a_1.webp',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_a_2.webp',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_a_3.webp',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_a_4.webp',
 
         ],
         name: 'Audífonos deportivos',
@@ -32,8 +32,8 @@ export const products = [
         id: 2,
         inStock: 20,
         images: [
-            '/earphones_b_1.png',
-            '/earphones_b_2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_b_1.webp',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_b_2.webp',
 
         ],
         name: 'Audifonos inalambricos',
@@ -51,8 +51,8 @@ export const products = [
         id: 3,
         inStock: 25,
         images: [
-            '/earphones_c_1.png',
-            '/earphones_c_2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_c_1.webp',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_c_2.webp',
 
         ],
         name: 'Audifonos Sports',
@@ -70,8 +70,8 @@ export const products = [
         id: 4,
         inStock: 10,
         images: [
-            '/headphones-a-1.png',
-            '/headphones-a-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/headphones-a-1.webp',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/headphones-a-2.webp',
         ],
         name: 'Audifonos Gamer',
         price: 250,
@@ -88,8 +88,8 @@ export const products = [
         id: 5,
         inStock: 20,
         images: [
-            '/headphones-b-1.png',
-            '/headphones-b-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676832882/tech%20store/headphones_b_1.webp',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676832882/tech%20store/headphones_b_2.webp',
         ],
         name: 'Audifonos Bass',
         price: 200,
@@ -106,8 +106,8 @@ export const products = [
         id: 6,
         inStock: 25,
         images: [
-            '/headphones-c-1.png',
-            '/headphones-c-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/headphones-c-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/headphones-c-2.png',
         ],
         name: 'Audifonos Bass',
         price: 200,
@@ -124,7 +124,7 @@ export const products = [
         id: 7,
         inStock: 25,
         images: [
-            '/iphone-13-pro-1.png'
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/iphone-13-pro-1.png'
         ],
         name: 'Iphone 13 Pro',
         price: 800,
@@ -141,8 +141,8 @@ export const products = [
         id: 8,
         inStock: 5,
         images: [
-            '/iphone-14-pro-1.png',
-            '/iphone-14-pro-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/iphone-14-pro-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776546/tech%20store/iphone-14-pro-2.png',
         ],
         name: 'Iphone 14 Pro',
         price: 1000,
@@ -159,8 +159,8 @@ export const products = [
         id: 9,
         inStock: 5,
         images: [
-            '/mac-studio-m1-1.png',
-            '/mac-studio-m1-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776543/tech%20store/mac-studio-m1-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/mac-studio-m1-2.png',
         ],
         name: 'Mac Studio M1',
         price: 1200,
@@ -177,8 +177,8 @@ export const products = [
         id: 10,
         inStock: 10,
         images: [
-            '/macbook-air-m2-1.png',
-            '/macbook-air-m2-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776546/tech%20store/macbook-air-m2-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776546/tech%20store/macbook-air-m2-2.png',
         ],
         name: 'MacBook Air M2',
         price: 900,
@@ -195,8 +195,8 @@ export const products = [
         id: 11,
         inStock: 14,
         images: [
-            '/oculus-quest-2-1.png',
-            '/oculus-quest-2-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/oculus-quest-2-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/oculus-quest-2-2.png',
         ],
         name: 'Oculus Quest 2',
         price: 600,
@@ -213,8 +213,8 @@ export const products = [
         id: 12,
         inStock: 20,
         images: [
-            '/playstation-4-1.png',
-            '/playstation-4-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-4-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-4-2.png',
         ],
         name: 'PlayStation 4 Slim', 
         price: 300,
@@ -231,8 +231,8 @@ export const products = [
         id: 13,
         inStock: 10,
         images: [
-            '/playstation-5-1.png',
-            '/playstation-5-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-5-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-5-2.png',
         ],
         name: 'PlayStation 5', 
         price: 550,
@@ -249,8 +249,8 @@ export const products = [
         id: 14,
         inStock: 10,
         images: [
-            '/playstation-vr-1.png',
-            '/playstation-vr-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-vr-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-vr-2.png',
         ],
         name: 'PlayStation VR', 
         price: 500,
@@ -267,8 +267,8 @@ export const products = [
         id: 15,
         inStock: 50,
         images: [
-            '/watch-1.png',
-            '/watch-2.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/watch-1.png',
+            'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/watch-2.png',
         ],
         name: 'SmartWatch Sport', 
         price: 70,
